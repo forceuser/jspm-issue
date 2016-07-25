c62fda3850ae2bb633cbba4a79ab5fb4b3e077e1
Vitaly Dub <forceuserz@gmail.com>
2016-07-25 14:03:17 +0000

    fix

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ##JSPM relative path resolving test
+
+start with
+
+```IP=127.0.0.1 PORT=8087 node index.js```
--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import module1 from './module1';
 console.log("module1", module1);
 
-System.import('./module2').then(module2 => {
+System.import('./module2.js').then(module2 => {
     console.log("module2", module2.default);
 });
